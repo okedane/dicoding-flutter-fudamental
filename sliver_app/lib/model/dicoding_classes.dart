643beb669555f85b9aa9a9c9c64600611e94ef03
dhanie <romadani.code@gmail.com>
2025-01-18 23:48:13 +0700

    Sliver

---
+++ sliver_app/lib/model/dicoding_classes.dart
@@ -0,0 +1,120 @@
+class DicodingCourse {
+  final String title;
+  final String description;
+
+  DicodingCourse({
+    required this.title,
+    required this.description,
+  });
+}
+
+final List<DicodingCourse> androidPath = [
+  DicodingCourse(
+    title: 'Belajar Fundamental Aplikasi Android',
+    description:
+        'Belajar membuat aplikasi Android dengan bahasa pemrograman Kotlin.',
+  ),
+  DicodingCourse(
+    title: 'Menjadi Android Developer Expert',
+    description: 'Belajar membuat aplikasi Android dengan arsitektur MVVM.',
+  ),
+  DicodingCourse(
+    title: 'Menjadi Game Developer Expert',
+    description: 'Belajar membuat game dengan Unity.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Android Jetpack Pro',
+    description: 'Belajar komponen-komponen Android Jetpack.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Android dengan Jetpack Compose',
+    description: 'Belajar membuat UI Android dengan Jetpack Compose.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Testing untuk Aplikasi Android',
+    description: 'Belajar melakukan testing pada aplikasi Android.',
+  ),
+];
+
+final List<DicodingCourse> iosPath = [
+  DicodingCourse(
+      title: 'Memulai Pemprograman Dengan Swift',
+      description: "Belajar dasar-dasar pemrograman Swift."),
+  DicodingCourse(
+    title: 'Belajar Fundamental iOS',
+    description: 'Belajar membuat aplikasi iOS dengan Swift.',
+  ),
+  DicodingCourse(
+    title: 'Menjadi iOS Developer Expert',
+    description: 'Belajar membuat aplikasi iOS dengan arsitektur MVVM.',
+  ),
+  DicodingCourse(
+    title: 'Belajar SwiftUI',
+    description: 'Belajar membuat UI iOS dengan SwiftUI.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Testing untuk Aplikasi iOS',
+    description: 'Belajar melakukan testing pada aplikasi iOS.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Core Data',
+    description: 'Belajar menggunakan Core Data untuk penyimpanan data di iOS.',
+  ),
+];
+
+final List<DicodingCourse> multiplatformPath = [
+  DicodingCourse(
+    title: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
+    description: 'Belajar membuat aplikasi Flutter untuk pemula.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
+    description: 'Belajar membuat aplikasi Flutter untuk pemula.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
+    description: 'Belajar membuat aplikasi Flutter untuk pemula.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
+    description: 'Belajar membuat aplikasi Flutter untuk pemula.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
+    description: 'Belajar membuat aplikasi Flutter untuk pemula.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
+    description: 'Belajar membuat aplikasi Flutter untuk pemula.',
+  ),
+];
+final List<DicodingCourse> webPath = [
+  DicodingCourse(
+    title: 'Belajar Dasar Pemrograman Web',
+    description:
+        'Belajar dasar-dasar pemrograman web dengan HTML, CSS, dan JavaScript.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Front-End Web untuk Pemula',
+    description:
+        'Belajar membuat tampilan website dengan framework seperti React atau Vue.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Membuat Back-End Web untuk Pemula',
+    description: 'Belajar membuat server dan API dengan Node.js atau Django.',
+  ),
+  DicodingCourse(
+    title: 'Menjadi Web Developer Expert',
+    description:
+        'Belajar membuat aplikasi web dengan arsitektur yang baik dan best practices.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Testing untuk Aplikasi Web',
+    description: 'Belajar melakukan testing pada aplikasi web.',
+  ),
+  DicodingCourse(
+    title: 'Belajar Progressive Web Apps',
+    description:
+        'Belajar membuat aplikasi web yang bisa diinstall seperti aplikasi native.',
+  ),
+];
---
+++ sliver_app/lib/widgets/listtile_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sliver_app/model/dicoding_classes.dart';
+
+class ListTileItem extends StatelessWidget {
+ final DicodingCourse dicodingCourse;
+ 
+ const ListTileItem({
+   super.key,
+   required this.dicodingCourse,
+ });
+ 
+ @override
+ Widget build(BuildContext context) {
+   return ListTile(
+     title: Text(dicodingCourse.title),
+     subtitle: Text(
+       dicodingCourse.description,
+       maxLines: 5,
+       overflow: TextOverflow.ellipsis,
+     ),
+     isThreeLine: true,
+     onTap: () {},
+   );
+ }
+}
